d7a489dde60b2a6b2f5afbc702f18a35a58cdc3f
Tristan Thompson <tristan15thompson@gmail.com>
2021-03-05 15:52:12 -0800

    [ADD] added tests and routes for professors

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,14 @@ const express = require('express');
 
 const authRoutes = require('./auth.js');
 const studentsRoutes = require('./students.js');
+const professorsRoutes = require('./professors.js');
+
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/students', studentsRoutes);
+router.use('/professors', professorsRoutes);
+
 
 module.exports = router;
